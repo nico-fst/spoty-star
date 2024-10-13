0f783c8b76dcebd09467f9065f488f24b2b8f350
Nico Stern <nst.web@posteo.de>
2024-10-13 23:55:38 +0200

    Get Currently Playing works

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # spoty-star
 A Spotify Flask Server automating spotify workflows
+
+# Debugging Nightmares
+
+- localhost bei redirect und /callback nicht gleich behandelt wie 127.0.0.1
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app.secret_key = os.getenv('SECRET_KEY')
 
 CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
-REDIRECT_URL = 'http://localhost:5000/callback'
+REDIRECT_URL = 'http://localhost:5001/callback'
 AUTH_URL = 'https://accounts.spotify.com/authorize'
 TOKEN_URL = 'https://accounts.spotify.com/api/token'
 SCOPE = 'user-read-playback-state user-read-currently-playing'
@@ -20,22 +20,22 @@ SCOPE = 'user-read-playback-state user-read-currently-playing'
 @app.route('/')
 def index():
     return 'Hello, melde dich an unter <a href="/login">Login</a>'
-    
+
 @app.route('/login')
 def login():
-    auth_url = f"{AUTH_URL}?response_type=code&client_id={CLIENT_ID}&scope={SCOPE}&redirect_uri={REDIRECT_URL}"
+    # login optional, debug: dass jedes Mal Anmeldung abgefragt
+    auth_url = f"{AUTH_URL}?response_type=code&client_id={CLIENT_ID}&scope={SCOPE}&redirect_uri={REDIRECT_URL}&prompt=login"
     return redirect(auth_url)
-    
+
 @app.route('/callback')
 def callback():
-    print("IN CALLBACK ANGEKOMMEN")
     # args werden von Spotify an die URL /callback angehangen
     code = request.args.get('code')
     
     if not code:
         return 'Fehler beim Login: Kein Authorizaton Code erhalten'
     
-    # Authorization Code -> Access token
+    # Exchange Authorization Code -> Access token
     token_resp = requests.post(TOKEN_URL, data={
         'grant_type': "authorization_code",
         'code': code,
@@ -61,16 +61,21 @@ def currently_playing():
         'Authorization': f'Bearer {token}'
     }
     
+    # -> resp.json()['item'] liefert album, artists, duration, ...
     resp = requests.get('https://api.spotify.com/v1/me/player/currently-playing', headers=headers)
     
     if resp.status_code == 200 and resp.json():
         track_data = resp.json()
+        
         track_name = track_data['item']['name']
         track_artist_name = track_data['item']['artists'][0]['name']
+        track_year = track_data['item']['album']['release_date'].split('-')[0]
         
-        return f'Aktuell läuft: {track_name} von {track_artist_name}'
+        return f'Aktuell läuft: {track_name} von {track_artist_name}, veröffentlicht im Jahr {track_year}'
     else:
         return 'Kein Song läuft gerade'
     
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="localhost", port=5001, debug=True)
